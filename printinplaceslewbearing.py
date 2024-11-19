f313f5f30325b90cb15dd08d9c6edce6e62e4c34
xBartM <mielczarek.cs@gmail.com>
2024-11-19 16:21:12 +0100

    roller_diameter is now calculated automatically

--- printinplaceslewbearing.py
+++ printinplaceslewbearing.py
@@ -1,11 +1,16 @@
 import cadquery as cq
+from math import sqrt, tan, pi
 
 class SlewBearing:
-    def __init__(self, inner_diameter, outer_diameter, width, roller_diameter, roller_fit, roller_slide, num_rollers):
+    def __init__(self, inner_diameter, outer_diameter, width, roller_fit, roller_slide, num_rollers):
         # Layer adhesion is probably a limiting factor for the strength of the bearing
-        # Points of failure are probably at 1/2 of width where there are the least amount of material
-        # To ensure as even load distribution at 1/2 of width the area of an inner ring should be equal to the area of the outer ring
+        # Points of failure are probably at 1/2 of width where there is the least amount of material
+        # To ensure even load distribution at 1/2 of width, the area of an inner ring should be equal to the area of the outer ring
         # This way we eliminate the need to choose the pitch_diameter by ourselves
+
+        # Since the bearing will be 3D printed, roller_diameter can be arbitrary
+        # Roller_diameter depends on pitch_diameter and num_rollers
+        # In order to reduce the number of less important variables, roller_diameter is calculated
 
         # Constraints
         ## 3d printer
@@ -18,8 +23,14 @@ class SlewBearing:
         
         # Consistency check
         ## positive values
-        if any(map(lambda x: x < 0.0, (inner_diameter, outer_diameter, width, roller_diameter, roller_fit, roller_slide, num_rollers))):
+        if any(map(lambda x: x < 0.0, (inner_diameter, outer_diameter, width, roller_fit, roller_slide, num_rollers))):
             raise ValueError('Value can\'t be less than 0.0.')
+
+        if num_rollers % 2 == 1:
+            raise ValueError('The number of rollers must be divisible by 2.')
+
+        roller_theta = pi/num_rollers # 360.0 / (num_rollers*2) to get an angle that a single roller occupies
+        roller_diameter = sqrt(2.0)*sqrt((outer_diameter*outer_diameter + inner_diameter*inner_diameter)/(tan(roller_theta)**(-2.0)+4.0)) - roller_fit
 
         ## pitch_diameter real and more than 0.0
         if outer_diameter**2.0 + inner_diameter**2.0 - (roller_diameter + roller_fit)**2.0 <= 0.0:
@@ -29,14 +40,14 @@ class SlewBearing:
 
         ## inner race as single piece
         if pitch_diameter - inner_diameter - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < inner_race_min_thickness:
-            raise ValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing pitch diameter or decreasing roller diameter+roller fit.')
+            raise ValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing outer diameter, decreasing roller fit or increasing the number of rollers.')
         ## outer race as single piece
         if outer_diameter - pitch_diameter - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < outer_race_min_thickness:
-            raise ValueError('Outer race min thickness too low. Consider decreasing pitch diameter, increasing outer diameter or decreasing roller diameter+roller fit.')
+            raise ValueError('Outer race min thickness too low. Consider increasing outer diameter, decreasing inner diameter, decreasing roller fit or increasing the number of rollers.')
 
         ## roller chamfer length
         if roller_diameter*sqrt(2.0) + roller_fit*sqrt(2.0)/2.0 - roller_slide*sqrt(2.0)/2.0 - width < roller_chamfer_min_length:
-            raise ValueError('Roller chamfer too small. Consider increasing roller diameter + roller fit, decreasing roller slide, decreasing bearing width')
+            raise ValueError('Roller chamfer too small. Consider increasing roller fit, decreasing roller slide, decreasing bearing width or decreasing the number of rollers.')
 
         # Parameters
         ## Races
@@ -45,6 +56,7 @@ class SlewBearing:
         self.pitch_diameter = pitch_diameter # diameter of the circle that that the rollers follow during rotation of a bearing
         self.width = width # bearing width
         ## Rollers
+        self.roller_theta = roller_theta # an angle from the middle of the roller to it's tangent neighbour
         self.roller_diameter = roller_diameter # diameter of a roller
         self.roller_fit = roller_fit # tolerances between races and rolling part of a roller
         self.roller_length = roller_diameter + roller_fit - roller_slide # length of a roller
@@ -170,8 +182,6 @@ class SlewBearing:
         )
         return side_section
 
-def sqrt(num):
-    return num**0.5
 
 # Create an instance of the SlewBearing
 # based on https://www.ldb-bearing.com/slewing-bearings/cross-roller-bearing
@@ -179,10 +189,10 @@ my_bearing = SlewBearing(
     inner_diameter=234,
     outer_diameter=403.5,
     width=45,
-    roller_diameter=38.9,
+#    roller_diameter=38.9,
     roller_fit=1.1,
     roller_slide=1.5,
-    num_rollers=12
+    num_rollers=52.0
 )
 
 #bearing_geometry = my_bearing.model
