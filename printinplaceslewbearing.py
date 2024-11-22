6dec59450d321fda03468a10c71df539b80d4f58
xBartM <mielczarek.cs@gmail.com>
2024-11-22 17:18:18 +0100

    infinite loop for generation all num_roller possibilities + custom exceptions

--- printinplaceslewbearing.py
+++ printinplaceslewbearing.py
@@ -41,10 +41,10 @@ class SlewBearing:
         # Consistency check
         ## positive values
         if any(map(lambda x: x < 0.0, (inner_radius, outer_radius, width, roller_fit, roller_slide, num_rollers))):
-            raise NegativeValueError('Value can\'t be less than 0.0.')
+            raise SlewBearing.NegativeValueError('Value can\'t be less than 0.0.')
 
         if num_rollers % 2 == 1:
-            raise OddRollerValueError('The number of rollers must be divisible by 2.')
+            raise SlewBearing.OddRollerValueError('The number of rollers must be divisible by 2.')
 
         roller_theta_rad = pi/num_rollers # 360.0 / (num_rollers*2) to get an angle that a single roller occupies
         roller_theta_deg = 360.0 / (num_rollers*2.0) # for use later
@@ -52,25 +52,27 @@ class SlewBearing:
 
         ## pitch_radius real and more than 0.0
         if outer_radius**2.0 + inner_radius**2.0 - (roller_diameter + roller_fit)**2.0 <= 0.0:
-            raise PitchRadiusValueError('Pitch radius not real.')
+            raise SlewBearing.PitchRadiusValueError('Pitch radius not real.')
 
         pitch_radius = (sqrt(2.0)/2.0)*sqrt(outer_radius**2.0 + inner_radius**2.0 - (roller_diameter + roller_fit)**2.0)
 
         ## inner race as single piece
         if pitch_radius - inner_radius - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < inner_race_min_thickness:
-            raise InnerRaceValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing outer diameter, decreasing roller fit or increasing the number of rollers.')
+            raise SlewBearing.InnerRaceValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing outer diameter, decreasing roller fit or increasing the number of rollers.')
         ## outer race as single piece
         if outer_radius - pitch_radius - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < outer_race_min_thickness:
-            raise OuterRaceValueError('Outer race min thickness too low. Consider increasing outer diameter, decreasing inner diameter, decreasing roller fit or increasing the number of rollers.')
+            raise SlewBearing.OuterRaceValueError('Outer race min thickness too low. Consider increasing outer diameter, decreasing inner diameter, decreasing roller fit or increasing the number of rollers.')
 
         ## roller chamfer length
         roller_chamfer_length = roller_diameter*sqrt(2.0) + roller_fit*sqrt(2.0)/2.0 - roller_slide*sqrt(2.0)/2.0 - width
         if roller_chamfer_length < roller_chamfer_min_length:
-            raise RollerChamferValueError('Roller chamfer too small. Consider increasing roller fit, decreasing roller slide, decreasing bearing width or decreasing the number of rollers.')
+            raise SlewBearing.RollerChamferValueError('Roller chamfer too small. Consider increasing roller fit, decreasing roller slide, decreasing bearing width or decreasing the number of rollers.')
 
         # Parameters
         ## Races
+        self.inner_diameter = inner_diameter # ID
         self.inner_radius = inner_radius # ID/2.0 of the bearing
+        self.outer_diameter = outer_diameter # OD
         self.outer_radius = outer_radius # OD/2.0 of the bearing
         self.pitch_radius = pitch_radius # radius of the circle that that the rollers follow during rotation of a bearing
         self.width = width # bearing width
@@ -174,6 +176,12 @@ class SlewBearing:
                 color=cq.Color(roller_colour)
                 )
         return assy
+    
+    def exportAssyStl(self, folder):
+        self.assy.toCompound().exportStl(folder 
+                                         + "b" + str(self.outer_diameter) + "x" + str(self.inner_diameter) + "x" + str(self.width) 
+                                         + "_" + str(self.roller_fit) + "x" + str(self.roller_slide) 
+                                         + "_" + str(self.num_rollers) + ".stl")
 
 
 # Create an instance of the SlewBearing
@@ -201,20 +209,17 @@ W  = 20.0
 RF = 0.3
 RS = 0.9
 NR = 2
+
 while True:
     try:
-        SlewBearing(OD, ID, W, RF, RS, NR)
+        SlewBearing(OD, ID, W, RF, RS, NR).exportAssyStl("models/")
+    except SlewBearing.RollerChamferValueError as e:
+        print("num_rollers=" + str(NR) + ": " + str(e))
         break
     except ValueError as e:
-        print(str(NR) + " doesn't work " + str(e))
+        print("num_rollers=" + str(NR) + ": " + str(e))
+    finally:
         NR += 2
-while True:
-    try:
-        SlewBearing(OD, ID, W, RF, RS, NR).assy.toCompound().exportStl("models/b" + str(OD) + "x" + str(ID) + "x" + str(W) + "_" + str(RF) + "x" + str(RS) + "_" + str(NR) + ".stl")
-        NR += 2
-    except ValueError:
-        break
-    
 
 # Display the bearing
 show_object(sample_bearing.assy)
