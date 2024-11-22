ca53a62fd2a0e2d86d41d64bc713fb4199c5ec26
xBartM <mielczarek.cs@gmail.com>
2024-11-22 16:54:00 +0100

    added chamfer to race edges + some misc code changes

--- printinplaceslewbearing.py
+++ printinplaceslewbearing.py
@@ -2,6 +2,21 @@ import cadquery as cq
 from math import sqrt, tan, pi
 
 class SlewBearing:
+
+    class NegativeValueError(ValueError):
+        pass
+    class OddRollerValueError(ValueError):
+        pass
+    class PitchRadiusValueError(ValueError):
+        pass
+    class InnerRaceValueError(ValueError):
+        pass
+    class OuterRaceValueError(ValueError):
+        pass
+    class RollerChamferValueError(ValueError):
+        pass
+
+    
     def __init__(self, outer_diameter, inner_diameter, width, roller_fit, roller_slide, num_rollers):
         # Layer adhesion is probably a limiting factor for the strength of the bearing
         # Points of failure are probably at 1/2 of width where there is the least amount of material
@@ -26,10 +41,10 @@ class SlewBearing:
         # Consistency check
         ## positive values
         if any(map(lambda x: x < 0.0, (inner_radius, outer_radius, width, roller_fit, roller_slide, num_rollers))):
-            raise ValueError('Value can\'t be less than 0.0.')
+            raise NegativeValueError('Value can\'t be less than 0.0.')
 
         if num_rollers % 2 == 1:
-            raise ValueError('The number of rollers must be divisible by 2.')
+            raise OddRollerValueError('The number of rollers must be divisible by 2.')
 
         roller_theta_rad = pi/num_rollers # 360.0 / (num_rollers*2) to get an angle that a single roller occupies
         roller_theta_deg = 360.0 / (num_rollers*2.0) # for use later
@@ -37,20 +52,21 @@ class SlewBearing:
 
         ## pitch_radius real and more than 0.0
         if outer_radius**2.0 + inner_radius**2.0 - (roller_diameter + roller_fit)**2.0 <= 0.0:
-            raise ValueError('Pitch radius not real.')
+            raise PitchRadiusValueError('Pitch radius not real.')
 
         pitch_radius = (sqrt(2.0)/2.0)*sqrt(outer_radius**2.0 + inner_radius**2.0 - (roller_diameter + roller_fit)**2.0)
 
         ## inner race as single piece
         if pitch_radius - inner_radius - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < inner_race_min_thickness:
-            raise ValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing outer diameter, decreasing roller fit or increasing the number of rollers.')
+            raise InnerRaceValueError('Inner race min thickness too low. Consider decreasing inner diameter, increasing outer diameter, decreasing roller fit or increasing the number of rollers.')
         ## outer race as single piece
         if outer_radius - pitch_radius - (roller_diameter + roller_fit) * sqrt(2.0)/2.0 < outer_race_min_thickness:
-            raise ValueError('Outer race min thickness too low. Consider increasing outer diameter, decreasing inner diameter, decreasing roller fit or increasing the number of rollers.')
+            raise OuterRaceValueError('Outer race min thickness too low. Consider increasing outer diameter, decreasing inner diameter, decreasing roller fit or increasing the number of rollers.')
 
         ## roller chamfer length
-        if roller_diameter*sqrt(2.0) + roller_fit*sqrt(2.0)/2.0 - roller_slide*sqrt(2.0)/2.0 - width < roller_chamfer_min_length:
-            raise ValueError('Roller chamfer too small. Consider increasing roller fit, decreasing roller slide, decreasing bearing width or decreasing the number of rollers.')
+        roller_chamfer_length = roller_diameter*sqrt(2.0) + roller_fit*sqrt(2.0)/2.0 - roller_slide*sqrt(2.0)/2.0 - width
+        if roller_chamfer_length < roller_chamfer_min_length:
+            raise RollerChamferValueError('Roller chamfer too small. Consider increasing roller fit, decreasing roller slide, decreasing bearing width or decreasing the number of rollers.')
 
         # Parameters
         ## Races
@@ -58,6 +74,9 @@ class SlewBearing:
         self.outer_radius = outer_radius # OD/2.0 of the bearing
         self.pitch_radius = pitch_radius # radius of the circle that that the rollers follow during rotation of a bearing
         self.width = width # bearing width
+        self.race_chamfer = line_thickness * 1 # chamfer for all but at 1/2 width edges
+        # well, maybe also add a minute chamfer at 1/2 width?
+
         ## Rollers
         self.roller_theta = roller_theta_deg # an angle from the middle of the roller to it's tangent neighbour
         self.roller_diameter = roller_diameter # diameter of a roller
@@ -65,6 +84,7 @@ class SlewBearing:
         self.roller_length = roller_diameter + roller_fit - roller_slide # length of a roller
         self.roller_slide = roller_slide # tolerances between races and sliding part of a roller
         self.num_rollers = num_rollers # number of the rollers - it needs to be divisible by two
+        self.roller_chamfer_length = roller_chamfer_length # length of the chamfer on the roller when viewing from the side
         
         ## model parts
         self.races = self.makeRaces()
@@ -89,6 +109,18 @@ class SlewBearing:
                        (((self.roller_diameter+self.roller_fit)*sqrt(2.0))/2.0, 0.0),
                        (0.0, -((self.roller_diameter+self.roller_fit)*sqrt(2.0))/2.0)
                        ]).close()
+            .revolve(axisStart=(self.pitch_radius, 0.0, 0.0),
+                     axisEnd=  (self.pitch_radius, 1.0, 0.0),
+                     combine='cut')
+            # add a chamfer on OD and ID
+            .faces(cq.NearestToPointSelector((0,0,self.width/2.0)))
+            .chamfer(self.race_chamfer)
+            .faces(cq.NearestToPointSelector((0,self.outer_radius,self.width/2.0)))
+            .chamfer(self.race_chamfer)
+            # break the edge between races
+            .center(0.0,
+                    0.0)
+            .rect(((self.roller_fit+self.roller_slide)*sqrt(2.0)/2.0+self.roller_chamfer_length)+self.race_chamfer*2.0, self.width, centered=True)
             .revolve(axisStart=(self.pitch_radius, 0.0, 0.0),
                      axisEnd=  (self.pitch_radius, 1.0, 0.0),
                      combine='cut')
@@ -146,7 +178,7 @@ class SlewBearing:
 
 # Create an instance of the SlewBearing
 # based on https://www.ldb-bearing.com/slewing-bearings/cross-roller-bearing
-my_bearing = SlewBearing(
+sample_bearing = SlewBearing(
     outer_diameter=403.5,
     inner_diameter=234,
     width=45,
@@ -156,11 +188,16 @@ my_bearing = SlewBearing(
     num_rollers=24
 )
 
-my_bearing.assy.toCompound().exportStl("models/bearing_test.stl")
+show_object(sample_bearing.races)
 
-OD = 50.0
-ID = 15.0
-W  = 10.0
+sample_bearing.assy.toCompound().exportStl("models/bearing_test.stl")
+
+OD = 200.0
+#OD = 50.0
+ID = 150.0
+#ID = 15.0
+W  = 20.0
+#W  = 10.0
 RF = 0.3
 RS = 0.9
 NR = 2
@@ -168,7 +205,8 @@ while True:
     try:
         SlewBearing(OD, ID, W, RF, RS, NR)
         break
-    except ValueError:
+    except ValueError as e:
+        print(str(NR) + " doesn't work " + str(e))
         NR += 2
 while True:
     try:
@@ -179,4 +217,4 @@ while True:
     
 
 # Display the bearing
-show_object(my_bearing.assy)
+show_object(sample_bearing.assy)
